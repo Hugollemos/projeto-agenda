84c340d0c81cd52b087c9bec8a87286e9c72a555
Hugo Lemos Da Silva <hugolemosdasilva.hu@gmail.com>
2021-05-28 08:52:27 -0300

    criando

---
+++ src/controllers/loginController.js
@@ -0,0 +1,15 @@
+const Login = require('../models/LoginModel')
+
+exports.index = (req, res) => {
+  res.render('login');
+};
+
+exports.register = async function(req, res) {
+  const login = new Login(req.body);
+  await login.register();
+  res.send(login.errors);
+
+  if(login.errors.length >0) {
+    req.flash('errors', login.errors);
+  }
+};
